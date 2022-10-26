6120a62a3903e0f83e48d20dd84ec84dea322a85
nvietcuong <cuongb1910499@student.ctu.edu.vn>
2022-10-26 10:22:31 +0700

    Xay dung trang cac san pham nguoi dung

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'ui/products/products_manager.dart';
 import 'ui/products/product_detail_screen.dart';
 import 'ui/products/product_overview_screen.dart';
+import 'ui/products/user_products_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -37,7 +38,7 @@ class MyApp extends StatelessWidget {
       // home: Container(
       // color: Colors.green,
       home: const SafeArea(
-        child: ProductsOverviewScreen(),
+        child: UserProductsScreen(),
       ),
     );
   }
