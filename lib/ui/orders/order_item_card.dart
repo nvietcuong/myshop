da3e75b734b62a40fbc3d7fecdbfc994a0408f59
nvietcuong <cuongb1910499@student.ctu.edu.vn>
2022-10-27 08:38:32 +0700

    Xay dung cac trang dat hang

--- lib/ui/orders/order_item_card.dart
+++ lib/ui/orders/order_item_card.dart
@@ -66,7 +66,7 @@ class _OrderItemCardState extends State<OrderItemCard> {
     return ListTile(
       title: Text('\$${widget.order.amount}'),
       subtitle: Text(
-        DateFormat('dd/MM/yyyy hh:mm').Form(widget.order.dateTime),
+        DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime),
       ),
       trailing: IconButton(
         icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
