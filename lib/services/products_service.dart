ee20e2a7769dd9a6edc21d00f49f8fc26f0a6b51
nvietcuong <cuongb1910499@student.ctu.edu.vn>
2022-11-01 18:01:32 +0700

    cap nhat , xoa san pham tren Realtime Database cua Firebase

---
+++ lib/services/products_service.dart
@@ -0,0 +1,106 @@
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:http/hhtp.dart' as http;
+import '../models/product.dart';
+import '../models/auth_token.dart';
+
+import 'firebase_service.dart';
+
+class ProductsService extends FirebaseService {
+  ProductsService([AuthToken? authToken]) : super(authToken);
+
+  Future<List<Product>> fetchProducts([bool filterByUser = false]) async {
+    final List<Product> products = [];
+
+    try {
+      final filters =
+          filterByUser ? 'orderBy="creatorId"&equalTo="$userId"' : '';
+      final productsUrl =
+          Uri.parse('$databaseUrl/products.json?auth=$token&$filters');
+      final response = await http.get(productsUrl);
+      final productsMap = json.decode(response.body) as Map<String, dynamic>;
+
+      if (response.statusCode != 200) {
+        print(productsMap['error']);
+        return products;
+      }
+
+      final userFavoritesUrl =
+          Uri.parse('$databaseUrl/userFavorites/$userId.json?auth=$token');
+      final userFavoritesResponse = await http.get(userFavoritesUrl);
+      final userFavoritesMap = json.decode(userFavoritesResponse.body);
+
+      productsMap.forEach((productId, product) {
+        final isFavorite = (userFavoritesMap == null)
+            ? false
+            : (userFavoritesMap[productId] ?? false);
+        products.add(
+          Product.fromJson({
+            'id': productId,
+            ...product,
+          }).copyWith(isFavorite: isFavorite),
+        );
+      });
+      return products;
+    } catch (error) {
+      print(error);
+      return products;
+    }
+  }
+
+  Future<Product?> addProduct(Product product) async {
+    try {
+      final url = Uri.parse('$databaseUrl/products.json?auth=$token');
+      final responses = await http.post(
+        url,
+        body: json.encode(
+          product.toJson()
+            ..addAll({
+              'creatorId': userId,
+            }),
+        ),
+      );
+      if (responses.statusCode != 200) {
+        throw Exception(json.decode(responses.body)['error']);
+      }
+      return product.copyWith(
+        id: json.decode(responses.body)['name'],
+      );
+    } catch (error) {
+      print(error);
+      return null;
+    }
+  }
+
+  Future<bool> updateProduct(Product product) async {
+    try {
+      final url = Uri.parse('$databaseUrl/{products.id}.json?auth=$token');
+      final respone = await http.patch(
+        url,
+        body: json.encode(product.toJson()),
+      );
+      if (respone.statusCode != 200) {
+        throw Exception(json.decode(respone.body)['error']);
+      }
+      return true;
+    } catch (error) {
+      print(error);
+      return false;
+    }
+  }
+
+  Future<bool> deleteProduct(String id) async {
+    try {
+      final url = Uri.parse('$databaseUrl/products/$id.json?auth=$token');
+      final respone = await http.delete(url);
+      if (respone.statusCode != 200) {
+        throw Exception(json.decode(respone.body)['error']);
+      }
+      return true;
+    } catch (error) {
+      print(error);
+      return false;
+    }
+  }
+}
